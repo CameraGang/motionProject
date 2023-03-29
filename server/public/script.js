7e3029d4adda93e2483886ba354a6261a6ad5293
Injun <ila19@sfu.ca>
2023-03-29 14:24:19 -0700

    add date and time in the webpage

--- server/public/script.js
+++ server/public/script.js
@@ -2,6 +2,7 @@ const socket = io();
 socket.on("connect", (socket) => { //confirm connection with NodeJS server
     console.log("Connected");
 });
+
 $(document).ready(function () {
     socket.on('canvas', function (data) {
         const canvas = $("#videostream");
@@ -14,4 +15,20 @@ $(document).ready(function () {
             context.drawImage(image, 0, 0, context.width, context.height);
         }
     });
+    showDate();
+    window.setInterval(function(){updateTime()}, 1000);
 });
+
+function updateTime() {
+	var now = new Date();
+	var timeStr = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+	$('#clock').html("Current Time : " + timeStr);
+}
+
+function showDate() {
+    const now = new Date();
+    const isoString = now.toISOString(); // e.g. '2022-04-06T14:30:00.000Z'
+    const [year, month, day] = isoString.slice(0, 10).split('-');
+    const formattedDate = `${year}:${month}:${day}`; // e.g. '2022:04:06'
+    $('#date').html("Date : " + formattedDate);
+}
--- server/routers/page.js
+++ server/routers/page.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const filePath = "/../public/";
 
-router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + filePath + "index.html"));
-});
-router.get('/script.js', function (req, res) {
-    res.sendFile(path.join(__dirname + filePath + "script.js"));
-});
+// Serve static files from the public directory
+router.use(express.static(path.join(__dirname, '..', 'public')));
 
 module.exports = router;
